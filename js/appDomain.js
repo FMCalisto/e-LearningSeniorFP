46913433710f7152f0c850d1866fc4c273823a8b
AnyaAlves <nyanaloveschocolatemilk@gmail.com>
2016-01-23 21:51:15 +0000

    delivered version

--- js/appDomain.js
+++ js/appDomain.js
@@ -76,7 +76,17 @@ var dbInit ="\
 <category name='Saúde'></category>\
 <category name='Tecnologia'></category>\
 </categories>\
-<courses></courses>\
+<courses>\
+<course>\
+<course_data id='COURSE0' category='Culinária' title='Como fazer bolos' description='Aprenda a fazer bolos' link='curso-bolos.html' img='./img/course8.png'></course_data>\
+</course>\
+<course>\
+<course_data id='COURSE1' category='Finanças' title='Análise de Mercados' description='Explore esta vertente económica' link='curso-analiseMercados.html' img='./img/course3.png'></course_data>\
+</course>\
+<course>\
+<course_data id='COURSE2' category='Línguas' title='Inglês' description='Lições de inglês básico' link='curso-ingles.html' img='./img/course7.png'></course_data>\
+</course>\
+</courses>\
 <current_user id='none'></current_user>\
 </app>";
 
@@ -183,7 +193,7 @@ function getCurrentUser() {
 	for (var i = 0; i < usersNodes.length; i++) {
 		userData = usersNodes[i].getElementsByTagName("user_data")[0];
 		if(userData.getAttribute("id") == currentUser.getAttribute("id")) {
-			return userData;
+			return usersNodes[i];
 		}
 	}
 	return null;
@@ -223,8 +233,14 @@ function registerUser() {
 	userData.setAttribute("email", inputEmail);
 	userData.setAttribute("password", inputPassword);
 	//userData.setAttribute(pictureUrl, someurl);
+
+	//if it's student
+	var student = document.createElement("student");
+	var studentCourses = document.createElement("student_courses");
 	//set user
+	student.appendChild(studentCourses);
 	user.appendChild(userData);
+	user.appendChild(student);
 	users.appendChild(user);
 
 	store(xmlDoc);
@@ -257,6 +273,36 @@ function createCourse() {
 	store(xmlDoc);
 }
 
+
+function enrollInCourse(courseId) {
+
+	var xmlDoc = load();
+	var currentUser = xmlDoc.getElementsByTagName("current_user")[0];
+	var users = xmlDoc.getElementsByTagName("users")[0];
+	var usersNodes = users.childNodes;
+	var currentUser = xmlDoc.getElementsByTagName("current_user")[0];
+	var user;
+
+	for (var i = 0; i < usersNodes.length; i++) {
+		userData = usersNodes[i].getElementsByTagName("user_data")[0];
+		if(userData.getAttribute("id") == currentUser.getAttribute("id")) {
+			user = usersNodes[i];
+			break;
+		}
+	}
+
+	
+	var studentCourses = user.getElementsByTagName("student")[0].getElementsByTagName("student_courses")[0];
+	var studentCourse = document.createElement("student_course");
+	studentCourse.setAttribute("id", courseId);
+
+	studentCourses.appendChild(studentCourse);
+
+	alert(xmlToString(xmlDoc));
+	store(xmlDoc);
+}
+
+
 function getCourseById(courseId) {
 	var xmlDoc = load();
 	var courses = xmlDoc.getElementsByTagName("courses")[0];
@@ -276,14 +322,23 @@ function getCourseById(courseId) {
 *******************************     DYNAMIC HTML    **************************************
 *****************************************************************************************/
 
+function submit() {
+	var url = window.location.href;
+	var i = url.indexOf("?");
+	if (i > -1) {
+		window.location.href = url.substring(0, i);
+	}
+}
+
 function updateNavbar() {
+	submit();
 	var leftLink = document.getElementById("navbarButtonLeft");
 	var rightLink = document.getElementById("navbarButtonRight");
 	if (userLoggedIn()) {
 		leftLink.setAttribute("href", "perfil.html");
 		leftLink.removeAttribute("data-toggle");
 		leftLink.removeAttribute("data-target");
-		leftLink.innerHTML = "Olá, "+ getCurrentUser().getAttribute("first_name");  
+		leftLink.innerHTML = "Olá, "+ getCurrentUser().getElementsByTagName("user_data")[0].getAttribute("first_name");  
 
 //NOT WORKING
 rightLink.setAttribute("href", "index.html");
@@ -313,11 +368,74 @@ $("#profileNav").show();
 
 
 function updateProfile() {
+	var xmlDoc = load();
+
 	var name = document.getElementById("userName");
 	var email = document.getElementById("userEmail");
+	var user = getCurrentUser().getElementsByTagName("user_data")[0];
 	
-	name.innerHTML = getCurrentUser().getAttribute("first_name") + " " + getCurrentUser().getAttribute("last_name");
-	email.innerHTML = getCurrentUser().getAttribute("email");  
+	name.innerHTML = user.getAttribute("first_name") + " " + user.getAttribute("last_name");
+	email.innerHTML = user.getAttribute("email"); 
+
+
+	//get all courses
+	var currentUser = xmlDoc.getElementsByTagName("current_user")[0];
+	var users = xmlDoc.getElementsByTagName("users")[0];
+	var usersNodes = users.childNodes;
+	var currentUser = xmlDoc.getElementsByTagName("current_user")[0];
+	var user;
+
+	for (var i = 0; i < usersNodes.length; i++) {
+		userData = usersNodes[i].getElementsByTagName("user_data")[0];
+		if(userData.getAttribute("id") == currentUser.getAttribute("id")) {
+			user = usersNodes[i];
+			break;
+		}
+	}
+
+
+	var studentCourses = user.getElementsByTagName("student")[0].getElementsByTagName("student_courses")[0];
+	var coursesNodes = studentCourses.childNodes;
+
+	for (var i = 0; i<coursesNodes.length; i++) {
+		var studentCourse = coursesNodes[i].getElementsByTagName("student_course")[0];
+		var courseId = studentCourse.getAttribute("id");
+
+
+		if(courseId == "bolos") {
+			$("#tab1-bolos").show();
+		}
+		else if(courseId == "mercados") {
+			$("#tab1-mercados").show();
+		} 
+		else if(courseId == "ingles") {
+			$("#tab1-ingles").show();
+		} 
+	}
+}
+
+
+
+function loadEnrolledCourses(categoryName) {
+	var coursesDiv = document.getElementById("courses");
+	var categoryDiv = document.createElement("div");
+	coursesDiv.appendChild(categoryDiv);
+	categoryDiv.setAttribute("class", "row");
+
+	var xmlDoc = load();
+	var courses = xmlDoc.getElementsByTagName("courses")[0];
+	var coursesNodes = courses.childNodes;
+
+
+	//load all courses from that category
+	for (var i = 0; i < coursesNodes.length; i++) {
+		courseData = coursesNodes[i].getElementsByTagName("course_data")[0];
+		courseCategory = courseData.getAttribute("category");
+		courseId = courseData.getAttribute("id");
+		if (courseCategory == categoryName) {
+			loadCoursesThumbnails(containerDiv, courseId);
+		}
+	}
 }
 
 
@@ -342,6 +460,8 @@ function loadCategories() {
 		liTag.setAttribute("onclick", "setActiveCategory(this.id)");
 		sideBarCategories.appendChild(liTag);
 	}
+
+	setActiveCategory("Todos");
 }
 
 function setActiveCategory(categoryName) {
@@ -351,7 +471,18 @@ function setActiveCategory(categoryName) {
 	category = document.getElementById(categoryName);
 	category.setAttribute("class", "active");
 
-	loadCourses(categoryName);
+	$('#courses').empty();
+	if (categoryName == 'Todos') {
+
+		loadCourses("Culinária");
+		loadCourses("Finanças");
+		loadCourses("Línguas");
+		loadCourses("Saúde");
+		loadCourses("Tecnologia");
+	}
+	else {
+		loadCourses(categoryName);
+	}
 
 
 	activeCategory = categoryName;
@@ -359,7 +490,6 @@ function setActiveCategory(categoryName) {
 }
 
 function loadCourses(categoryName) {
-	$('#courses').empty();
 	var coursesDiv = document.getElementById("courses");
 	var categoryDiv = document.createElement("div");
 	coursesDiv.appendChild(categoryDiv);
@@ -396,10 +526,10 @@ function loadCoursesThumbnails(categoryDiv, courseId) {
 	courseThumbnail.innerHTML ="\
 	         <div class='col-md-4 portfolio-item' >\
                 <a href='curso-exemplo.html'>\
-                    <img class='img-responsive' src='./img/course8.png' alt=''/>\
+                    <img class='img-responsive' src=" + course.getAttribute("img") + " alt=''/>\
                 </a>\
                 <h3>\
-                    <a href='curso-exemplo.html'>"+course.getAttribute("title")+"</a>\
+                    <a href="+course.getAttribute("link") + ">"+course.getAttribute("title")+"</a>\
                 </h3>\
                 <p>"+course.getAttribute("description")+"</p>\
             </div>";
